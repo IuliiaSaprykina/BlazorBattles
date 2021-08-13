9f6944d0979b4a995cb4341fb9d6d8807b67bbe2
rr829347 <rr829347@FL3078AM124937>
2021-08-13 16:36:23 -0400

    Add project files.

---
+++ BlazorBattles/Client/Pages/Counter.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorBattles.Client.Pages
+{
+    public partial class Counter:ComponentBase
+    {
+       
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
